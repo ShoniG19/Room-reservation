bd331b6c45c6d963b2d1acefc3afb993e7d05bc7
ShoniG19 <jonigodoy19.jg@gmail.com>
2025-06-18 20:23:06 -0300

    modifica api para aceptar variables de .env

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.BACK_URL || "http://localhost:9891/api";
+const API_URL = process.env.BACK_URL;
 
 const api = axios.create({
   baseURL: API_URL,
